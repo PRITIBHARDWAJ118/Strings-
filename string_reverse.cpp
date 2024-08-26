41df9fab06c77c4f97771c55cdcd91c83513f0b9
Priti <pritibhardwaj118@gmail.com>
2024-08-27 00:15:04 +0530

    Update string_reverse.cpp

--- string_reverse.cpp
+++ string_reverse.cpp
@@ -5,11 +5,11 @@
 using namespace std;
 int main()
 {
-    string str1, str2;
+    string str1;
     int i,len;
     cout<<"Enter a string: ";
     cin>>str1;
-    len= str1.length();
+    len= str1.length();//length of string 
      cout<<"Reversed string is : ";
     for (i=(len-1);i>=0;i--)
     {
